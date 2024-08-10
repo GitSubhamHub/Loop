cdd0017916efcd0a779c13f0b9f0562a64953a93
Subham <132269477+GitSubhamHub@users.noreply.github.com>
2024-08-10 22:22:52 +0530

    Add files via upload

---
+++ infinte_while_loop.js.js
@@ -0,0 +1,22 @@
+// Example: Infinte Loop
+
+// console.log("Before Loop");
+// var count = 0;
+// while (true) {
+//     console.log("Welcome to Ducat", ++count);
+// }
+// console.log("After Loop");
+
+
+//How to save your self from infinite loop?
+//Solution: Condition
+
+var num = 1
+console.log("Before Loop");
+while (num <= 40) {
+    console.log("Welcome to Ducat"+num);
+    num=num+1
+}
+console.log("After Loop");
+
+
---
+++ loop.js
@@ -0,0 +1,23 @@
+//Loop: it is a control statement which is used to execute same line until the given condition is true.
+
+//in the case of loop three things are important:
+//startingPoint: decides from where you need to start your loop
+//EndingPoint: How many times you need to run your code.
+//Condition: it is used to control/stop/start your loop
+
+//there are following types of loop in javascript
+//while loop:
+//do-while loop:
+//for loop:
+
+//loop: something is in running condition.
+
+//while loop: Used to execute same lines of code until the given condition is true.
+/*
+syntax: 
+
+while(condtion){
+   //lines of code if condition is true.
+}
+
+*/
